2fe56f765765ee8ffadf717a86ae006b05d18d82
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-11-02 19:43:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
       API_ID = "10247139"
       API_HASH = "96b46175824223a33737657ab943fd6a"
-      BOT_TOKEN = "5222572158:AAENHtTOnhWBh4UUZKTjq5ruMtil_4zRA_0"
+      BOT_TOKEN = "5222572158:AAGwMiAMGgj9BmMQdcxn58Cq19stEnoVarI"
