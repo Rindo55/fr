7c2203a236c6c59585099187607a8e465b0071c2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-01-06 22:46:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def start(bot, cmd: Message):
         await cmd.reply_text("Bot seems online! ⚡️")
     else:
         try:
-            if client.get_chat_member(-1001159872623, message.from_user.id).status == "member":
+            if client.get_chat_member(-1001159872623, cmd.from_user.id).status == "member":
                 file_id = int(b64_to_str(usr_cmd).split("_")[-1])
                 file_id = int(usr_cmd.split("_")[-1])
                 GetMessage = await app.get_messages(kay_id, message_ids=file_id)
